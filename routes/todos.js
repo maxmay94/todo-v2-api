e3e67617cba6d4373b9f8f672842b2ca942b1b98
maxmay94 <maxmay94@gmail.com>
2022-06-04 11:42:43 -0400

    readme

--- routes/todos.js
+++ routes/todos.js
@@ -8,12 +8,10 @@ const router = Router()
 router.get('/', todoCtrl.index)
 router.patch('/:id/done', todoCtrl.markDone)
 
-
 /* ------------------ Private Routes ------------------ */
 router.use(decodeUserFromToken)
 router.post('/', checkAuth, todoCtrl.create)
 router.patch('/:id', checkAuth, todoCtrl.update)
 router.delete('/:id', checkAuth, todoCtrl.delete)
-
 
 export { router }
